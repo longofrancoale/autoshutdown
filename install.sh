18aaabfc99dc347735af7f4d7adecd95e62296a9
longofrancoale <40793698+longofrancoale@users.noreply.github.com>
2020-12-30 18:48:52 +0100

    Wgagw

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 cp ./auto-shutdown.service /etc/systemd/system/
 mkdir /.scripts
 cp autoshutdown /.scripts
+echo "sleep 1" >> /.scripts/stop
+chmod +x /.scripts/stop
 echo "Inserisci il tempo di spegnimento cosí \n (ora):(mimuto)"
 read input
 echo "$input" >> /.scripts/shutdowntime
